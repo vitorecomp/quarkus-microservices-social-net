78778dbc020b31661c7a9abd2e2a612f10b49a76
vitorecomp <vitor.ecomp@gmail.com>
2023-04-18 13:20:10 -0300

    feat: create the base

--- utilities/scripts/openshift-deploy/deploy-monitoring.sh
+++ utilities/scripts/openshift-deploy/deploy-monitoring.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+
+#create the monitoring project
+oc new-project monitoring
+
+#intall the prometheus operator
+
+#intall the crds
+
+
+#install the grafana operator
+
+
+#intall the crds
+
+#create all the dashboards
